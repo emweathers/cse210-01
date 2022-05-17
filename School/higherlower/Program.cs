3e41be16d55a3084c1c5ff83c280e4c2fccba674
E Weathers <104738787+emweathers@users.noreply.github.com>
2022-05-17 10:26:34 -0600

    Update Program.cs

--- School/higherlower/Program.cs
+++ School/higherlower/Program.cs
@@ -130,7 +130,7 @@ namespace higherlower {
             Player a = new Player();
             Card c = new Card();
             c.NewCard();
-            a.autoretry = true; // Comment this line out if you do not wish to automate the process, or uncomment it if you want it to be automated
+            //a.autoretry = true; // Comment this line out if you do not wish to automate the process, or uncomment it if you want it to be automated
             while(a.GetPoints() > 0 && a.GetRetry().ToLower() == "y") {
                 c.DisplayCard();
                 c.NewCard();
